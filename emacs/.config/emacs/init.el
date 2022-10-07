4b12d04ab1c54adb6b123a4639f752d91d3abf76
Gabriel Lisaca <gabriel.lisaca@gmail.com>
2022-10-07 18:46:52 +1300

    emacs: Use https for package archives
    Why did the blog author recommend http links?

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,9 +1,9 @@
 ;;; From https://blog.aaronbieber.com/2015/05/24/from-vim-to-emacs-in-fourteen-days.html
 (require 'package)
 
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
 ;; (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
