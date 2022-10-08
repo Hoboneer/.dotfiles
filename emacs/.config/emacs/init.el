271a2f67e4c3b9ff489f7f47df8682cf9c96a51d
Gabriel Lisaca <gabriel.lisaca@gmail.com>
2022-10-08 14:34:48 +1300

    emacs: Use melpa
    Strangely enough, not all packages on melpa are on melpa-stable.  I
    need this for corfu-doc.  If only it was on stable or part of corfu
    itself.

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -2,7 +2,7 @@
 (require 'package)
 
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
-;; (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
 (unless (package-installed-p 'use-package)
