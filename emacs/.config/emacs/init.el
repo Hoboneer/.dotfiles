fa459811ff7d285f0241317dff798f945ca18240
Gabriel Lisaca <gabriel.lisaca@gmail.com>
2022-12-13 14:41:40 +1300

    emacs: Add current buffer target for embark from the wiki

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -517,7 +517,35 @@ Uses `consult-completion-in-region'."
   :init
   (setq prefix-help-command #'embark-prefix-help-command)
   :config
+  ;; From https://github.com/oantolin/embark/wiki/Additional-Actions#use-embark-like-a-leader-key
+  (defun embark-target-this-buffer-file ()
+    (cons 'this-buffer-file (or (buffer-file-name) (buffer-name))))
+  (add-to-list 'embark-target-finders #'embark-target-this-buffer-file 'append)
+  (add-to-list 'embark-keymap-alist '(this-buffer-file . this-buffer-file-map))
+  (embark-define-keymap this-buffer-file-map
+    "Commands to act on current file or buffer."
+    ("l" load-file)
+    ("b" byte-compile-file)
+    ;; Not defined on my system.
+    ;; ("S" sudo-find-file)
+    ;; ("r" rename-file-and-buffer)
+    ("d" diff-buffer-with-file)
+    ("=" ediff-buffers)
+    ("C-=" ediff-files)
+    ("!" shell-command)
+    ("&" async-shell-command)
+    ("x" consult-file-externally)
+    ("c" copy-file)
+    ("k" kill-buffer)
+    ("z" bury-buffer)
+    ("|" embark-shell-command-on-buffer)
+    ("g" revert-buffer))
+  (defun embark-act-on-buffer-file (&optional arg)
+    (interactive "P")
+    (let ((embark-target-finders '(embark-target-this-buffer-file)))
+      (embark-act arg)))
   :bind* (("C-." . embark-act)
+	  ("C-M-." . embark-act-on-buffer-file)
 	  ("M-." . embark-dwim)
 	  ("C-h B" . embark-bindings)))
 (use-package embark-consult
